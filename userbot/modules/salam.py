0b63efcaa10173cc8e694f79350a6a4477abb022
Camel07 <72527434+Camel07@users.noreply.github.com>
2021-01-04 02:38:41 +0800

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -7,6 +7,25 @@ from userbot.events import register
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
+@register(outgoing=True, pattern='^G(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit(f"**JAKA SEMBUNG BAWA GOLOK**")
+    sleep(3)
+    await typew.edit("`NIMBRUNG GOBLOKK!!!`")
+# Owner @Si_Dian
+
+
+@register(outgoing=True, pattern='^g(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit(f"**JAKA SEMBUNG BAWA GOLOK**")
+    sleep(3)
+    await typew.edit("`NIMBRUNG GOBLOKK!!!`")
+# Owner @Si_Dian
+
 
 @register(outgoing=True, pattern='^P(?: |$)(.*)')
 async def typewriter(typew):
@@ -51,7 +70,8 @@ async def typewriter(typew):
 CMD_HELP.update({
     "salam":
     "`P`\
-\nUsage: Untuk Memberi salam.\
+\nUsage: Untuk Memberi Hujatan.\
 \n\n`L`\
 \nUsage: Untuk Menjawab Salam."
-})
+\n\n`G`\
+\nUsage: Untuk nyuruh nimbrung anak kontok.")}

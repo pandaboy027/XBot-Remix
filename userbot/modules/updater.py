767b3812944ef79d8a0e681eacb960f1a460ffe4
Camel07 <72527434+Camel07@users.noreply.github.com>
2021-01-17 21:04:36 +0800

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -214,10 +214,10 @@ async def upstream(event):
         await event.edit(
             '`Force-Syncing to latest stable userbot code, please wait...`')
     else:
-        await event.edit('`Proses Update XBOT-REMIX, ....🛠️`')
-        await event.edit('`Proses Update XBOT-REMIX, loading....🛠️`')
-        await event.edit('`Proses Update XBOT-REMIX, updating....🛠️`')
-        await event.edit('`Proses Update XBOT-REMIX, silahkan tunggu....🛠️`')
+        await event.edit('`Proses Update BOT KAMPANG🐨, ....🛠️`')
+        await event.edit('`Proses Update BOT KAMPANG🐨, loading....🛠️`')
+        await event.edit('`Proses Update BOT KAMPANG🐨, updating....🛠️`')
+        await event.edit('`Proses Update BOT KAMPANG🐨 silahkan tunggu kontol....🛠️`')
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
         await asyncio.sleep(10)

879b9194b188c85ec8586bf7ebebad172e66dfda
Koala <72527434+Camel07@users.noreply.github.com>
2021-01-31 08:54:24 +0800

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -1,69 +1,65 @@
-"""Globally Ban users from all the
-Group Administrations bots where you are SUDO
-Available Commands:
-.gban REASON
-.ungban REASON"""
-import asyncio
-from userbot.events import register
-from userbot import CMD_HELP, bot, G_BAN_LOGGER_GROUP, ALIVE_NAME
-# imported from uniborg by @heyworld
+"""
+credits to @mrconfused and @sandy1709
+"""
+#    Copyright (C) 2020  sandeep.n(π.$)
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from sqlalchemy import Column, String
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
-
-
-@register(outgoing=True, pattern="^.gban(?: |$)(.*)")
-async def _(event):
-    if G_BAN_LOGGER_GROUP is None:
-        await event.edit("Set G_BAN_LOGGER_GROUP in vars otherwise module won't work.")
-        return
-    if event.fwd_from:
-        return
-    reason = event.pattern_match.group(1)
-    if event.reply_to_msg_id:
-        r = await event.get_reply_message()
-        if r.forward:
-            r_from_id = r.forward.from_id or r.from_id
-        else:
-            r_from_id = r.from_id
-        await bot.send_message(
-            G_BAN_LOGGER_GROUP,
-            "/gban [user](tg://user?id={}) {}".format(r_from_id, reason)
-        )
-    await event.reply("**gbanning...**")
-    await event.edit(f"**User gbanned by {DEFAULTUSER}**")
-    asyncio.sleep(5)
-    await event.delete()
+from userbot.plugins.sql_helper import BASE, SESSION
 
 
-@register(outgoing=True, pattern="^.ungban(?: |$)(.*)")
-async def _(event):
-    if G_BAN_LOGGER_GROUP is None:
-        await event.edit("Set G_BAN_LOGGER_GROUP in vars otherwise module won't work.")
-        return
-    if event.fwd_from:
-        return
-    reason = event.pattern_match.group(1)
-    if event.reply_to_msg_id:
-        r = await event.get_reply_message()
-        r_from_id = r.from_id
-        await bot.send_message(
-            G_BAN_LOGGER_GROUP,
-            "/ungban [user](tg://user?id={}) {}".format(r_from_id, reason)
-        )
-    await event.delete()
-    await event.reply("**ungbanning...**")
-    asyncio.sleep(3.5)
-    await event.edit(f"**User ungbanned by {DEFAULTUSER}**")
-    asyncio.sleep(5)
-    await event.delete()
+class GBan(BASE):
+    __tablename__ = "gban"
+    chat_id = Column(String(14), primary_key=True)
+    reason = Column(String(127))
+
+    def __init__(self, chat_id, reason=""):
+        self.chat_id = chat_id
+        self.reason = reason
 
 
-CMD_HELP.update({
-    "gban": "\
-`.gban reason`\
-\nUsage: Globally Ban users from all the Group Administrations bots where you are SUDO.\
-\n\n`.ungban reason`\
-\nUsage: Globally unBan users from all the Group Administrations bots where you are SUDO"
-})
+GBan.__table__.create(checkfirst=True)
+
+
+def is_gbanned(chat_id):
+    try:
+        return SESSION.query(GBan).filter(GBan.chat_id == str(chat_id)).one()
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+
+def get_gbanuser(chat_id):
+    try:
+        return SESSION.query(GBan).get(str(chat_id))
+    finally:
+        SESSION.close()
+
+
+def catgban(chat_id, reason):
+    adder = GBan(str(chat_id), str(reason))
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def catungban(chat_id):
+    rem = SESSION.query(GBan).get(str(chat_id))
+    if rem:
+        SESSION.delete(rem)
+        SESSION.commit()
+
+
+def get_all_gbanned():
+    rem = SESSION.query(GBan).all()
+    SESSION.close()
+    return rem

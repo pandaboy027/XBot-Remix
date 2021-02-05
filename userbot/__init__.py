363721dd1b4bf922bc19eebda5d6e4ea730fb8f8
Koala <72527434+Camel07@users.noreply.github.com>
2021-02-05 08:05:49 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -341,11 +341,11 @@ ZALG_LIST = {}
 
 def paginate_help(page_number, loaded_modules, prefix):
     number_of_rows = 5
-    number_of_cols = 4
+    number_of_cols = 2
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {}".format("🐨", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {}".format("🔹", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
